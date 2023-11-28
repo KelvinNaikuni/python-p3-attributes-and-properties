f0783630a2eb5f3f0a6f27ac32fe8b2a2e36d44e
Kelvin Naikuni <kelvin.naikuni@student.moringaschool.com>
2023-11-28 18:19:34 +0300

    complete

--- lib/testing/dog_test.py
+++ lib/testing/dog_test.py
@@ -10,7 +10,7 @@ class TestDog:
 
     def test_is_class(self):
         '''is a class with the name "Dog".'''
-        fido = Dog()
+        fido = Dog(name="Fido")
         assert(type(fido) == Dog)
         
     def test_name_not_empty(self):
@@ -19,7 +19,7 @@ class TestDog:
         sys.stdout = captured_out
         Dog(name="")
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Name must be string between 1 and 25 characters.\n")
+        assert(captured_out.getvalue() == "Name must be a string between 1 and 25 characters.\n")
 
     def test_name_string(self):
         '''prints "Name must be string between 1 and 25 characters." if not string.'''
@@ -27,7 +27,7 @@ class TestDog:
         sys.stdout = captured_out
         Dog(name=123)
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Name must be string between 1 and 25 characters.\n")
+        assert(captured_out.getvalue() == "Name must be a string between 1 and 25 characters.\n")
 
     def test_name_under_25(self):
         '''prints "Name must be string between 1 and 25 characters." if string over 25 characters.'''
@@ -35,7 +35,7 @@ class TestDog:
         sys.stdout = captured_out
         Dog(name="What do dogs do on their day off? Can't lie around - that's their job.")
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Name must be string between 1 and 25 characters.\n")
+        assert(captured_out.getvalue() == "Name must be a string between 1 and 25 characters.\n")
 
     def test_valid_name(self):
         '''saves name if string between 1 and 25 characters.'''
@@ -46,11 +46,11 @@ class TestDog:
         '''prints "Breed must be in list of approved breeds." if not in breed list.'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
-        Dog(breed="Human")
+        Dog(name="Fido", breed="Human")
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Breed must be in list of approved breeds.\n")
+        assert(captured_out.getvalue() == "Breed must be in the list of approved breeds.\n")
 
     def test_breed_in_list(self):
         '''saves breed if in breed list.'''
-        fido = Dog(breed="Pug")
+        fido = Dog(name="Fido", breed="Pug")
         assert(fido.breed == "Pug")
